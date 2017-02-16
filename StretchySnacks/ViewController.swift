db186c26d67e59812b23fb9aebc611cd49506ad6
Jonester <chris.jones78@gmail.com>
2017-02-16 18:09:22 -0500

    Initial setup

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plusButton(_ sender: UIButton) {
+        print("plus icon pushed")
+    }
 
 }
 
