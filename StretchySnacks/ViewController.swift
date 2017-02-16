a1664fb9178a4e4044416d668be140c74f76dfb7
Jonester <chris.jones78@gmail.com>
2017-02-16 17:56:02 -0500

    Initial Commit

---
+++ StretchySnacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchySnacks
+//
+//  Created by Chris Jones on 2017-02-16.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
